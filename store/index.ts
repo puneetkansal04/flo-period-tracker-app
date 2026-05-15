de1bc758d6da29eb55343e87db5bf65693e08749
Puneet <48505993+puneetkansal04@users.noreply.github.com>
2026-05-15 11:25:52 +0530

    Build: Simplify Redux store and increase CI memory limits

--- store/index.ts
+++ store/index.ts
@@ -1,27 +1,20 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import periodReducer from './slices/periodSlice';
 
-const periodPersistConfig = {
-  key: 'period',
-  storage: AsyncStorage,
-  blacklist: ['isSessionUnlocked'],
-};
-
-const rootReducer = combineReducers({
-  period: persistReducer(periodPersistConfig, periodReducer),
-});
-
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
+  blacklist: ['isSessionUnlocked'], // We only have one slice anyway
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, periodReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    period: persistedReducer,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
